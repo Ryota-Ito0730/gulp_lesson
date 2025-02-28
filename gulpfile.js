eccc079f2709f69f08e8fca47a37a578c85a2db9
ryota ito <ryota@ryotanoMacBook-Air.local>
2025-02-28 11:36:13 +0900

    gulp.js内の記述をES Modulesに変更

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,53 @@
 // gulpプラグインを読み込みます
-const { src, dest, watch } = require("gulp");
+import gulp from "gulp";
 
 // SassやPugをコンパイルするプラグインを読み込みます
-const sass = require("gulp-sass")(require('sass'));
-const pug = require("gulp-pug");
+import *  as dartSass from "sass";
+import gulpSass from "gulp-sass";
+const sass = gulpSass(dartSass);
+import pug from "gulp-pug";
 
 /**
  * Sass(SCSS)をコンパイルするタスクです
  */
 const compileSass = () => {
-// cssフォルダ直下の全てのscssファイルを取得
-return src("css/*.scss")
-  // Sassのコンパイルを実行
-  .pipe(
-    // コンパイル後のCSSを展開
-    sass({
-      outputStyle: "expanded"
-    })
-  )
-  // cssフォルダー以下に保存
-  .pipe(dest("css"));
+  // cssフォルダ直下の全てのscssファイルを取得
+  return gulp.src("css/*.scss")
+    // Sassのコンパイルを実行
+    .pipe(
+      // コンパイル後のCSSを展開
+      sass({
+        outputStyle: "expanded"
+      })
+    )
+    // cssフォルダー以下に保存
+    .pipe(gulp.dest("css"));
 }
 
 /**
  * Pugをコンパイルするタスクです
  */
 const compilePug = () => {
-// pugフォルダ直下の全てのPugファイルを取得
-return src("pug/*.pug")
-  // コンパイルを実行
-  .pipe(
-    // コンパイル後のpugを展開
-    pug({
-        pretty: true
-    })
-  )
-  // pugフォルダー以下に保存
-  .pipe(dest("pug"));
+  // pugフォルダ直下の全てのPugファイルを取得
+  return gulp.src("pug/*.pug")
+    // コンパイルを実行
+    .pipe(
+      // コンパイル後のpugを展開
+      pug({
+          pretty: true
+      })
+    )
+    // pugフォルダー以下に保存
+    .pipe(gulp.dest("pug"));
 }
 
 /**
  * 各ファイルを監視し、変更があったらSassやHTMLを変換します
  */
 const watchFiles = () => {
-  watch("css/*.scss", compileSass)
-  watch("pug/*.pug", compilePug)   
+  gulp.watch("css/*.scss", compileSass)
+  gulp.watch("pug/*.pug", compilePug)
 }
 
 // npx gulpというコマンドを実行後、watchFilesが実行されるようにします
-exports.default = watchFiles;
+export default gulp.series(watchFiles);
